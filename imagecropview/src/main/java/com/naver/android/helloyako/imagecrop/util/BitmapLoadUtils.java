582bb7b95f88b1026676f811de03d29e874045a6
helloyako <helloyako@gmail.com>
2016-12-23 13:59:07 +0900

    remove getPathFromUri in BitmapLoadUtils

--- imagecropview/src/main/java/com/naver/android/helloyako/imagecrop/util/BitmapLoadUtils.java
+++ imagecropview/src/main/java/com/naver/android/helloyako/imagecrop/util/BitmapLoadUtils.java
@@ -100,27 +100,6 @@ public class BitmapLoadUtils {
     }
 
 
-    public static String getPathFromUri(Context context, Uri uri) {
-        if (uri == null) {
-            return null;
-        }
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(uri, proj, null, null, null);
-            if (cursor == null) {
-                return null;
-            }
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
-
     private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight, boolean useImageView) {
         // Raw height and width of image
         final int height = options.outHeight;
